3b431925398187c07c0f851ede1fccd96f768d5a
BiancaVVV <63341026+BiancaVVV@users.noreply.github.com>
2025-08-28 23:18:03 +0300

    dependencies added

---
+++ moneymanagerapp/src/main/java/com/bianca/moneymanagerapp/MoneymanagerappApplication.java
@@ -0,0 +1,13 @@
+package com.bianca.moneymanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymanagerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymanagerappApplication.class, args);
+	}
+
+}
